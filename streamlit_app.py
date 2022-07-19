4233593535511d5c4e93f7e49b4e774f68f6a117
nikampratiksha <68655538+nikampratiksha@users.noreply.github.com>
2022-07-19 15:07:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ streamlit.title('My parent new healthy dinner')
 streamlit.text('breakfast menu')
 streamlit.text('🐔 good food')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
